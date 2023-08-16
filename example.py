2411eb7e835efa632dc935c15425e36ef0b68ab2
sKapshuk <32719332+sKapshuk@users.noreply.github.com>
2023-08-16 17:55:31 +0300

    Update example.py
    edid example

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from app import app
 class Config:
     SECRET_KEY = "test-secret"
 
-    SQLALCHEMY_DATABASE_URI = "postgresql://<<user>>:<<password>>@<<host>>:<<port>>/postgres"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '') or "postgresql://<<user>>:<<password>>@<<host>>:<<port>>/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     CATEGORY_UPLOAD_FOLDER = os.path.join(app.root_path, 'static', 'img', 'category')
