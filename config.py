dea4c9b132cc420d2e787f0a4e4fc226d2837b23
sKapshuk <32719332+sKapshuk@users.noreply.github.com>
2023-08-16 19:55:07 +0300

    Update config.py
    commit change

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 from app import app
 
 class Config:
-    SECRET_KEY = "test-secret"
+    SECRET_KEY = os.environ['SECRET_KEY']
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '') or "postgresql://<<user>>:<<password>>@<<host>>:<<port>>/postgres"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     CATEGORY_UPLOAD_FOLDER = os.path.join(app.root_path, 'static', 'img', 'category')
